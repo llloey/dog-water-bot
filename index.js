eea88a16d077eeaf1cf53cf2cd44d32ab22e2cdc
llloey <jeerapong.pn@gmail.com>
2026-02-19 12:35:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Dog Water Bot is running 🐶");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 const { Client, GatewayIntentBits } = require("discord.js");
 const { google } = require("googleapis");
 
